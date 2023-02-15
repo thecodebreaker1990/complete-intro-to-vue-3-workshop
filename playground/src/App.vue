8ec8d84e16584a672fc84b5f52871065ac86ba0b
Alapan Chatterjee <alapan.chatterjee@cyware.com>
2023-02-15 21:39:41 +0530

    chore: setup data fetching & dynamic component registration

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,3 +1,41 @@
+<script>
+import UserCardList from "./components/UserCardList.vue";
+import UserTable from "./components/UserTable.vue";
+
+export default {
+  components: {
+    UserCardList,
+    UserTable,
+  },
+  data() {
+    return {
+      userList: [],
+      activeView: "card-list",
+    };
+  },
+  computed: {
+    viewComponent() {
+      return `user-${this.activeView}`;
+    },
+  },
+  methods: {
+    async fetchUsers() {
+      this.userList = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
+    },
+  },
+  created() {
+    this.fetchUsers();
+  },
+};
+</script>
+
 <template>
-  <h2>New Scratchpad</h2>
+  <h2>UserList</h2>
+  <div>
+    <button @click="activeView = 'card-list'">Card View</button>
+    <button @click="activeView = 'table'">Table View</button>
+  </div>
+  <component :is="viewComponent" :user-list="userList" />
 </template>
